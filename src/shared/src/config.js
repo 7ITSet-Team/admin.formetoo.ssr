9c3fade669248b4023279a6ba6579206b7c690a5
dournesh <dournesh@gmail.com>
2018-08-08 13:58:13 +0300

    add link to website with product

--- src/shared/src/config.js
+++ src/shared/src/config.js
@@ -1,13 +1,13 @@
-/*
+
 let config = {
 	uri: {
 		admin: 'http://admin.formetoo.ru/api',
 		login: 'http://admin.formetoo.ru/api/login',
 		allowed: 'http://admin.formetoo.ru/api/allowed'
 	}
-}*/
+}
 
-
+/*
 let config = {
 	uri: {
 		admin: 'http://localhost:3001/api',
@@ -15,6 +15,6 @@ let config = {
 		allowed: 'http://localhost:3001/api/allowed'
 	}
 }
-
+*/
 
 export default config
